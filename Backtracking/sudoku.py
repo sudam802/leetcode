73642a90418f98e4fa11124c16bc74d97409e950
sudam802 <57206830+sudam802@users.noreply.github.com>
2024-06-19 23:41:17 +0530

    Update sudoku.py

--- Backtracking/sudoku.py
+++ Backtracking/sudoku.py
@@ -1,1 +1,39 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+# checking in rows
+ 
+        for row in board :
+            seen=set()
+            for w in row:
+                if w!=".":
+                    if(w not in seen):
+                        seen.add(w)
+                    else:
+                        return False
+# checking for  columns
+        for col in range(9):
+            seen=set()
 
+            for row in  range(9):
+                d=board[row][col]
+                if d!=".":
+                    if(d not in seen):
+                        seen.add(d)
+                    else:
+                        return False
+ # checking for box
+        for i in range(3):
+            for j in range(3):
+                seen = set()
+                for x in range(i * 3, i * 3 + 3):
+                    for y in range(j * 3, j * 3 + 3):
+                        d=board[x][y]
+                        if d != ".":
+                            if d not in seen:
+                                seen.add(d)
+                            else:
+                                return False
+
+        return True
+
+        
